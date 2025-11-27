43b9e377fdef262521c45e4d3d46d89da56d2a31
ritvikchouhan77 <ritvikchouhan77@gmail.com>
2025-11-28 00:48:31 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,7 +1,13 @@
 package demo;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,7 +18,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class TestCases {
-    ChromeDriver driver;
+    static ChromeDriver driver;
     public TestCases()
     {
         System.out.println("Constructor: TestCases");
@@ -45,12 +51,97 @@ public class TestCases {
 
     }
 
+    public static void scrollToElement(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        String url = "https://leetcode.com/";
+        driver.get(url);
+        if(driver.getCurrentUrl().contains("leetcode")){
+            System.out.println("Test Case 01: PASS - URL contains 'leetcode'");
+        }else{
+            System.out.println("Test Case 01: FAIL - URL does not contain 'leetcode'");
+        }
+        System.out.println("end Test case: testCase01");
+    }
+    public static void testCase02() throws InterruptedException {
+        driver.get("https://leetcode.com/");
+        WebElement questionsLink = driver.findElement(By.xpath("//a[@href='/problemset/']"));
+        questionsLink.click();
+        String currentUrl = driver.getCurrentUrl();
+        if (currentUrl.contains("problemset")) {
+            System.out.println("Test Case 02: PASS - URL contains 'problemset'");
+        } else {
+            System.out.println("Test Case 02: FAIL - URL does not contain 'problemset'");
+        }
+        WebElement ele = driver.findElement(By.xpath("//div[@class='inline-block min-w-full']"));
+
+        scrollToElement(driver,ele);
+        Thread.sleep(5000);
+        // Retrieve details of the first 5 questions and print them
+        List<WebElement> questionElements = driver.findElements(By.xpath("//div[@role='rowgroup']/div/div[2]"));
+        System.out.println("First 5 Questions:");
+        for (int i = 1; i <= 5; i++) {
+            WebElement questionTitleElement = questionElements.get(i).findElement(By.xpath(".//descendant::a"));
+            String questionTitle = questionTitleElement.getText();
+            if(i==1){
+                if(questionTitle.contains("Two Sum")){
+                    System.out.println("First question is Verified Successfully");
+                }else{
+                    System.out.println("Failed to verify first question");
+                }
+            }
+            System.out.println(questionTitle);
+        }
     }
 
+    public static void testCase03(){
+        String c_url = driver.getCurrentUrl();
+        String url = "https://leetcode.com/problemset/";
+        if(!c_url.equals(url)){
+            driver.get(url);
+        }
+        List<WebElement> q_urls = driver.findElements(By.xpath("//div[@role='rowgroup']/div/div[2]//descendant::a"));
+
+        for(int i=0;i<q_urls.size();i++){
+            String title = q_urls.get(i).getText();
+            if(title.contains("Two Sum")){
+                q_urls.get(i).click();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+                break;
+            }
+        }
+//        driver.switchTo().frame(0);
+//        driver.findElement(By.xpath("//div[@class='modal-container']/span[text()='I don't want to subscribe!']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        if(driver.getCurrentUrl().contains("two-sum")){
+            System.out.println("The URL of the problem contains two-sum");
+            System.out.println("Verified Successfully...");
+        }
+
+    }
+
+    public static void testCase04(){
+        String c_url = driver.getCurrentUrl();
+        String url = "https://leetcode.com/problems/two-sum/description/";
+        if(!c_url.equals(url)){
+            driver.get(url);
+        }
+        driver.findElement(By.id("submissions_tab")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+            WebElement btn = driver.findElement(By.xpath("//a[text()='Register or Sign In']"));
+            if(btn.getText().equals("Register or Sign In")){
+                System.out.println("The message Register or Sign In is displayed when you click on the submissions tab.");
+            }else{
+                System.out.println("Register or Sign In button Verfication Failed ...");
+            }
+
+    }
 
 }
